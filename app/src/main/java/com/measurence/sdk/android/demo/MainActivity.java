148c2645af720b78846b0e384c4e5f5cb8acd971
Piercarlo Slavazza <pslavazza@measurence.com>
2014-09-18 13:51:04 +0200

    added copyright notices

--- app/src/main/java/com/measurence/sdk/android/demo/MainActivity.java
+++ app/src/main/java/com/measurence/sdk/android/demo/MainActivity.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Measurence Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.measurence.sdk.android.demo;
 
 import android.app.AlertDialog;
@@ -12,7 +36,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -20,7 +43,6 @@ import com.measurence.sdk.android.api_subscription.MeasurenceApiSubscriptionServ
 import com.measurence.sdk.android.api_subscription.gcm.AndroidApiSubscriptionService;
 import com.measurence.sdk.android.api_subscription.gcm.GCMRegistrationIdUtil;
 import com.measurence.sdk.android.api_subscription.httppost.HttpPostApiSubscriptionService;
-import com.measurence.sdk.android.gcm_push_notifications.PresenceSessionUpdatesNotificationService;
 
 import java.util.ArrayList;
 import java.util.List;
